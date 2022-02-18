c9db44d63d4f778e207893f590028baf476e7758
Aravind_Kumar <aravindkumar@trioangle.com>
2022-02-18 17:25:59 +0530

    SafeAreaViewAdded

--- CustomSafeAreaLayout/CustomSafeAreaLayout.docc/Resources/SafeAreaView.swift
+++ CustomSafeAreaLayout/CustomSafeAreaLayout.docc/Resources/SafeAreaView.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import UIKit
+
+class SafeAreaView: UIView {
+
+    @IBOutlet var topSafeAreaView: UIView!
+    @IBOutlet var bottomSafeAreaView: UIView!
+    @IBOutlet var mainAreaView: UIView!
+    
+    override func awakeFromNib() {
+       
+    }
+    
+    func setFrameForTopSafeAreaView(_ parentFrame: CGRect) -> CGRect{
+        topSafeAreaView.frame = parentFrame
+        return topSafeAreaView.frame
+    }
+    
+    func setFrameForBottomSafeAreaView(_ parentFrame: CGRect) -> CGRect{
+        bottomSafeAreaView.frame = parentFrame
+        return bottomSafeAreaView.frame
+    }
+    
+    class func initViewFromXib()-> SafeAreaView{
+        let nib = UINib(nibName: "SafeAreaView", bundle: nil)
+        let view = nib.instantiate(withOwner: nil, options: nil)[0] as! SafeAreaView
+        return view
+    }
+
+}
