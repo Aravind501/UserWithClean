55ced8873ac5d4fe17e4e190f7365bc01d999e4e
Aravind_Kumar <aravindkumar@trioangle.com>
2022-02-18 16:15:12 +0530

    initCommit

---
+++ CustomSafeAreaLayout/CustomSafeAreaLayout.h
@@ -0,0 +1,18 @@
+//
+//  CustomSafeAreaLayout.h
+//  CustomSafeAreaLayout
+//
+//  Created by trioangle on 18/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomSafeAreaLayout.
+FOUNDATION_EXPORT double CustomSafeAreaLayoutVersionNumber;
+
+//! Project version string for CustomSafeAreaLayout.
+FOUNDATION_EXPORT const unsigned char CustomSafeAreaLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomSafeAreaLayout/PublicHeader.h>
+
+
